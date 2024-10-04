6e509c599e415c54a097d0eafa5a20366fe020ea
Yanina Bellini Saibene <yabellini@gmail.com>
2024-10-04 10:02:42 -0300

    Agrego documentacion del conjunto de datos.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Conjunto de datos de ejemplo
+#'
+#' Los datos son utilizados como ejemplo de nuestra funcion de suma.
+#'
+#' @format Los datos son generados de forma sintetica.
+#' \describe{
+#'  \item{a}{muestra de datos}
+#'  \item{b}{muestra de datos multiplicada por cinco}
+#' }
+#' @source generacion propia con funciones de R
+"datos"
+
