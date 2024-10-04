90ab95739f46c7f5202451c7e3ca971fe54f7ee1
Paola Corrales <paobcorrales@gmail.com>
2024-10-04 22:31:53 +1000

    Documenta funcion suma

--- R/suma.R
+++ R/suma.R
@@ -1,5 +1,21 @@
-# Suma dos numeros
-
+#' Suma dos numeros
+#'
+#' La función `suma()` toma 2 números positivos y los suma.
+#'
+#' Estos son detalles extras de la función.
+#'
+#' @param x un vector numérico positivo
+#' @param y un vector numérico
+#'
+#' @return
+#' Un vector numérico con la suma de `x` e `y`.
+#'
+#' @examples
+#' suma(2, 2)
+#' suma(1, 10)
+#'
+#'
+#' @export
 suma <- function(x, y) {
   return(x + y)
 }
